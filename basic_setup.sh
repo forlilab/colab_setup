814a5774be1370211e351fe32424a2b55b8d2712
Amy He <ayhe1768@gmail.com>
2025-01-27 15:30:41 -0800

    fix URL, beautify setup.sh

--- basic_setup.sh
+++ basic_setup.sh
@@ -9,17 +9,17 @@ error() {
   echo -e "\n\033[1;31m[ERROR]\033[0m $1\n" >&2
 }
 
-# Step 1: Install Python packages with conda and pip
 log "Step 1: Installing Python packages using Conda and Pip..."
-if conda env update --name base --file colab_setup/basic_colab_env.yaml --prune; then
+yaml_file="colab_setup/basic_colab_env.yaml"
+if conda env update --name base --file ${yaml_file} --prune; then
   log "Python packages installed successfully."
 else
   error "Failed to install Python packages. Ensure Conda is installed and the YAML file exists."
   exit 1
 fi
 
-# Step 2: Clone the Phenix Project geostd (restraint) Library
-log "Step 2: Cloning Phenix Project geostd (restraint) Library..."
+log "Step 2: Cloning data repositories..."
+# Phenix Project geostd (restraint) library
 goestd_repo="https://github.com/phenix-project/geostd.git"
 if git clone ${goestd_repo}; then
   log "Phenix Project geostd repository cloned successfully."
@@ -28,29 +28,17 @@ else
   exit 1
 fi
 
-# Step 3: Download AutoDock Vina Executable
-log "Step 3: Downloading AutoDock Vina executable..."
-
-# Get the latest release version
+log "Step 3: Downloading executables..."
+# AutoDock Vina executable
 pinned_release="1.2.5"
-download_url="https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/$pinned_release/vina_${pinned_release}_linux_x86_64"
-
-# Download the executable
+download_url="https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v$pinned_release/vina_${pinned_release}_linux_x86_64"
 if wget -q --show-progress ${download_url}; then
+  # Rename and make the executable ready
+  mv vina_*_linux_x86_64 vina && chmod +x vina
   log "AutoDock Vina executable downloaded successfully."
 else
   error "Failed to download AutoDock Vina executable. Verify the release version or URL."
   exit 1
 fi
 
-# Rename and make the executable ready
-log "Renaming and setting permissions for the executable..."
-if mv vina_*_linux_x86_64 vina && chmod +x vina; then
-  log "Executable renamed to 'vina' and made executable successfully."
-else
-  error "Failed to rename or set permissions for the Vina executable."
-  exit 1
-fi
-
-# Final message
 log "Colab environment setup completed successfully!"
