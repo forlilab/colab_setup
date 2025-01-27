965a800d9818bca4acbc87cf339df63fd0b7e653
Amy He <ayhe1768@gmail.com>
2025-01-27 15:08:03 -0800

    pin vina to v1.2.5 in setup.sh

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# @title (2) Install Packages and Data (~ 5min)
-%%time
-
 # Install Python packages by conda and pip
 conda env update --name base --file colab_setup/basic_colab_env.yaml --prune
 
@@ -13,7 +10,7 @@ git clone ${goestd_repo}
 # Download AutoDock Vina Executable from Git Repository
 
 # Get the latest release version
-latest_release=$(curl -s https://api.github.com/repos/ccsb-scripps/AutoDock-Vina/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+pinned_release="1.2.5"
 # Construct the download URL
 wget https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/$latest_release/vina_${latest_release#v}_linux_x86_64
 # Rename and make it executable
