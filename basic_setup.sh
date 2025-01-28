7d42ceac0a9f0ba3ec7a01145ba17b0c6f263cef
Amy He <ayhe1768@gmail.com>
2025-01-27 17:58:50 -0800

    multiple edits: (1) add autogrid (2) modulize functions

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Function to display messages
-log() {
-  echo -e "\n\033[1;32m[INFO]\033[0m $1\n"
-}
-
-error() {
-  echo -e "\n\033[1;31m[ERROR]\033[0m $1\n" >&2
-}
+source colab_setup/lib/logging.sh
+source colab_setup/lib/download.sh
+source colab_setup/lib/checkout_from_repo.sh
 
 log "Step 1: Installing Python packages using Conda and Pip..."
 yaml_file="colab_setup/basic_colab_env.yaml"
@@ -21,24 +17,10 @@ fi
 log "Step 2: Cloning data repositories..."
 # Phenix Project geostd (restraint) library
 goestd_repo="https://github.com/phenix-project/geostd.git"
-if git clone ${goestd_repo}; then
-  log "Phenix Project geostd repository cloned successfully."
-else
-  error "Failed to clone Phenix Project geostd repository. Check your network connection."
-  exit 1
-fi
+get_repo "$goestd_repo" "geostd"
 
 log "Step 3: Downloading executables..."
-# AutoDock Vina executable
-pinned_release="1.2.5"
-download_url="https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v$pinned_release/vina_${pinned_release}_linux_x86_64"
-if wget -q --show-progress ${download_url}; then
-  # Rename and make the executable ready
-  mv vina_*_linux_x86_64 vina && chmod +x vina
-  log "AutoDock Vina executable downloaded successfully."
-else
-  error "Failed to download AutoDock Vina executable. Verify the release version or URL."
-  exit 1
-fi
+download_autodock_vina "1.2.5"
+download_autodock_gpu "1.6"
 
 log "Colab environment setup completed successfully!"
